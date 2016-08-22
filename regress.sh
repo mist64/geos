57a549b3405f44b74dc69fdfd580372d5eb4e1f1
Michael Steil <mist64@mac.com>
2016-08-21 18:04:02 -0700

    ...

--- regress.sh
+++ regress.sh
@@ -1,9 +1,15 @@
 reference=$1
 
-for file in kernal joydrv drv1541; do
+dd if=/dev/zero bs=1 count=48960 of=tmp.bin
+cat kernal.bin >> tmp.bin
+
+dd if=/dev/zero bs=1 count=48960 of=reference/$reference/tmp.bin
+cat reference/$reference/kernal.bin >> reference/$reference/tmp.bin
+
+for file in tmp; do
 	if [ -e reference/$reference/$file.bin ]; then
 		hexdump -C $file.bin > /tmp/$file.txt
 		hexdump -C reference/$reference/$file.bin > /tmp/reference_$file.txt
-		diff -u /tmp/reference_$file.txt /tmp/$file.txt | head -n 90
+		diff --suppress-common-lines -y /tmp/reference_$file.txt /tmp/$file.txt | head -n 10
 	fi
 done
