ba9d4c2bb948ef216a46780c9fb391353acb5e42
Michael Steil <mist64@mac.com>
2016-08-22 08:31:52 -0700

    ...

--- kernal/memory.s
+++ kernal/memory.s
@@ -237,7 +237,7 @@ _MoveData:
         lda     $03                             ; CE38 A5 03                    ..
         cmp     $05                             ; CE3A C5 05                    ..
         bne     @X                           ; CE3C D0 04                    ..
-        lda     r2L                           ; CE3E A5 02                    ..
+        lda     r0L                           ; CE3E A5 02                    ..
         cmp     $04                             ; CE40 C5 04                    ..
 @X:	bcc     @8                              ; CE42 90 2E                    ..
 .else
