79572caba1b29e13c44786e9c3769b54ccdda040
Michael Steil <mist64@mac.com>
2016-09-01 22:34:10 -0700

    ...

--- regress.sh
+++ regress.sh
@@ -8,8 +8,9 @@ dd if=/dev/zero bs=1 count=40320 of=reference/$reference/tmp.bin
 cat reference/$reference/lokernal.bin /dev/zero | dd bs=1 count=8640 >> reference/$reference/tmp.bin
 cat reference/$reference/kernal.bin >> reference/$reference/tmp.bin
 
-for file in tmp; do
+for file in tmp reu0 reu1 reu2 reu3 reu4 reu5 reu6 reu7 reu8 reu9 reu10 reu11; do
 	if [ -e reference/$reference/$file.bin ]; then
+		echo $file
 		hexdump -C $file.bin > /tmp/$file.txt
 		hexdump -C reference/$reference/$file.bin > /tmp/reference_$file.txt
 		diff --suppress-common-lines -y /tmp/reference_$file.txt /tmp/$file.txt | head -n 10
