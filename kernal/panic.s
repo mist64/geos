f1b8ce5f2fbb93da202053b455d15388043f0ffa
Michael Steil <mist64@mac.com>
2016-09-02 07:10:48 -0700

    ...

--- kernal/panic.s
+++ kernal/panic.s
@@ -173,13 +173,11 @@ _Panic:
 .endif
 
 _PanicDB_DT:
-.if wheels
-        .byte DEF_DB_POS | 1, DBTXTSTR, TXT_LN_X, TXT_LN_1_Y
-	.byte $38,$CF,$0E
-.else
 	.byte DEF_DB_POS | 1
 	.byte DBTXTSTR, TXT_LN_X, TXT_LN_1_Y
 	.word _PanicDB_Str
+.if wheels
+	.byte $0E
 .endif
 	.byte NULL
 
