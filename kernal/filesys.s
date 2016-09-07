ff049dc52e1e130b7ae55f329a51278b525eead3
Michael Steil <mist64@mac.com>
2016-09-07 16:33:14 +0200

    ...

--- kernal/filesys.s
+++ kernal/filesys.s
@@ -56,19 +56,32 @@
 .global _WriteFile
 .global _WriteRecord
 
-.segment "files1"
 
-.if !wheels_external_readwrite_file ; this has moved to the nerkernal"
+.if wheels_external_readwrite_file
+.segment "reu3"
+.else
+.segment "files1"
 Add2:
 	AddVW 2, r6
 return:
 	rts
+.endif
 
 ASSERT_NOT_BELOW_IO
 
+.if wheels_external_readwrite_file
+OReadFile:
+.else
 _ReadFile:
+.endif
 	jsr EnterTurbo
+.if wheels
+	beqx @X
+	rts
+@X:
+.else
 	bnex return
+.endif
 	jsr InitForIO
 	PushW r0
 	LoadW r4, diskBlkBuf
@@ -80,6 +93,9 @@ _ReadFile:
 	lda diskBlkBuf
 	bne @2
 	ldy diskBlkBuf+1
+.if wheels
+	beq @6
+.endif
 	dey
 	beq @6
 @2:	lda r2H
@@ -91,11 +107,20 @@ _ReadFile:
 	bne @7
 @3:	sty r1L
 	LoadB CPU_DATA, RAM_64K
+.if wheels
+	lda r7H
+	cmp #$4F
+	bcc @4
+	cmp #$52
+	bcs @4
+	jsr L50A4
+	bra @Y
+.endif
 @4:	lda diskBlkBuf+1,y
 	dey
 	sta (r7),y
 	bne @4
-	LoadB CPU_DATA, KRNL_IO_IN
+@Y:	LoadB CPU_DATA, KRNL_IO_IN
 	AddB r1L, r7L
 	bcc @5
 	inc r7H
@@ -114,6 +139,171 @@ _ReadFile:
 @7:	PopW r0
 	jmp DoneWithIO
 
+.if wheels
+L50A4:	lda r10L
+	pha
+	lda r9H
+	pha
+	lda r9L
+	pha
+	lda r3H
+	pha
+	lda r3L
+	pha
+	lda r2H
+	pha
+	lda r2L
+	pha
+	lda r1H
+	pha
+	lda r1L
+	pha
+	lda r0H
+	pha
+	lda r0L
+	pha
+	ldx #$00
+	sty r10L
+	lda r7H
+	sta r9H
+	lda r7L
+	sta r9L
+L50D1:	lda r9H
+	cmp #$50
+	bne L50DB
+	lda r9L
+	cmp #$00
+L50DB:	bcc L50E9
+	lda r9H
+	cmp #$51
+	bne L50E7
+	lda r9L
+	cmp #$C2
+L50E7:	bcc L5102
+L50E9:	ldy #$00
+	lda $8002,x
+	sta (r9L),y
+	clc
+	lda #$01
+	adc r9L
+	sta r9L
+	bcc L50FB
+	inc r9H
+L50FB:	inx
+	cpx r10L
+	bcc L50D1
+	bcs L512D
+L5102:	jsr L514F
+	clc
+	lda r9L
+	adc r2L
+	sta r9L
+	lda r9H
+	adc r2H
+	sta r9H
+	clc
+	lda r0L
+	adc r2L
+	bcs L512D
+	tax
+	dex
+	dex
+	cpx r10L
+	bcs L512D
+	ldy #$00
+L5122:	lda $8002,x
+	sta (r9L),y
+	iny
+	inx
+	cpx r10L
+	bcc L5122
+L512D:	pla
+	sta r0L
+	pla
+	sta r0H
+	pla
+	sta r1L
+	pla
+	sta r1H
+	pla
+	sta r2L
+	pla
+	sta r2H
+	pla
+	sta r3L
+	pla
+	sta r3H
+	pla
+	sta r9L
+	pla
+	sta r9H
+	pla
+	sta r10L
+	rts
+
+L514F:	sec
+	lda r9L
+	sbc #$00
+	sta r1L
+	lda r9H
+	sbc #$50
+	sta r1H
+	txa
+	clc
+	adc #$02
+	sta r0L
+	lda #$80
+	sta r0H
+	stx r2L
+	sec
+	lda r10L
+	sbc r2L
+	sta r2L
+	lda #$00
+	sta r2H
+	clc
+	lda r1L
+	adc r2L
+	sta r3L
+	lda r1H
+	adc r2H
+	sta r3H
+	lda r3H
+	cmp #$01
+	bne L518A
+	lda r3L
+	cmp #$C2
+L518A:	bcc L51A6
+	sec
+	lda r3L
+	sbc #$C2
+	sta r3L
+	lda r3H
+	sbc #$01
+	sta r3H
+	sec
+	lda r2L
+	sbc r3L
+	sta r2L
+	lda r2H
+	sbc r3H
+	sta r2H
+L51A6:	clc
+	lda #$27
+	adc r1L
+	sta r1L
+	lda #$06
+	adc r1H
+	sta r1H
+	lda $88C3
+	sta r3L
+	inc $88C3
+	jsr StashRAM
+	dec $88C3
+	rts
+.endif
+
+.if 0
 FlaggedPutBlock:
 	lda verifyFlag
 	beq @1
