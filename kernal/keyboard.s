f77256886df96fd7d02154de77b3ebc3224a9dfd
Michael Steil <mist64@mac.com>
2016-09-11 07:14:22 +0200

    ...

--- kernal/keyboard.s
+++ kernal/keyboard.s
@@ -186,13 +186,13 @@ KbdScanHelp1:
 .segment "keyboard2"
 
 KbdTab1:
-.if wheels
+.if german_keyboard
 	.byte $bb, $bb, $bb, $bb, $bb, $bb, $bb, $ba, $e0
 .else
 	.byte $db, $dd, $de, $ad, $af, $aa, $c0, $ba, $bb
 .endif
 KbdTab2:
-.if wheels
+.if german_keyboard
 	.byte $3c, $3c, $3c, $3c, $3c, $3c, $3c, $3e, $5e
 .else
 	.byte $7b, $7d, $7c, $5f, $5c, $7e, $60, $7b, $7d
@@ -200,7 +200,7 @@ KbdTab2:
 KbdTestTab:
 	.byte $fe, $fd, $fb, $f7, $ef, $df, $bf, $7f
 KbdDecodeTab1:
-.if wheels
+.if german_keyboard
 	.byte KEY_DELETE, CR, KEY_RIGHT, KEY_F7, KEY_F1, KEY_F3, KEY_F5, KEY_DOWN
 	.byte "3", "w", "a", "4", "y", "s", "e", KEY_INVALID
 	.byte "5", "r", "d", "6", "c", "f", "t", "x"
@@ -220,7 +220,7 @@ KbdDecodeTab1:
 	.byte "1", KEY_LARROW, KEY_INVALID, "2", " ", KEY_INVALID, "q", KEY_STOP
 .endif
 KbdDecodeTab2:
-.if wheels
+.if german_keyboard
 	.byte KEY_INSERT, CR, BACKSPACE, KEY_F8, KEY_F2, KEY_F4, KEY_F6, KEY_UP
 	.byte "@", "W", "A", "$", "Y", "S", "E", KEY_INVALID
 	.byte "%", "R", "D", "&", "C", "F", "T", "X"
